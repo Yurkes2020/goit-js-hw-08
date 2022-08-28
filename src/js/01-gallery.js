dd26eed22dc0cab819466506e15bb0c53fc21b5d
Yurkes2020 <yurkes2020@gmail.com>
2022-08-28 17:59:56 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const createGalleryItem = ({ preview, original, description }) => {
+  return `
+    <div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    </div>`;
+};
+
+const createGalleryMarkup = () => {
+  const galleryMarkup = galleryItems.map(createGalleryItem).join('');
+  galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+};
+
+createGalleryMarkup();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
